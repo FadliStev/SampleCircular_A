08fcb13ee527003cd666665cf820c291cd9c15c2
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-12-14 16:31:55 +0700

    Add project files.

---
+++ SampleCircular_A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCircular_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
