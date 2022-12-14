e4b2e40085362b1e63c936d724b5fdebc0d1387a
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-12-14 17:19:26 +0700

    end

--- SampleCircular_A/Program.cs
+++ SampleCircular_A/Program.cs
@@ -20,14 +20,14 @@ namespace SampleCircular_A
         public void insert(int element)
         {
             /*THis statement checks for the overflow condition*/
-            if((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1))
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
             {
                 Console.WriteLine("\nQueue Overflow");
                 return;
             }
             /*The following statement checks whether the queue is empty. If the
              queue is empty, then the value of the REAR and FRONT variables is set to 0.*/
-            if(FRONT == -1)
+            if (FRONT == -1)
             {
                 FRONT = 0;
                 REAR = 0;
@@ -36,11 +36,11 @@ namespace SampleCircular_A
             {
                 /*IF REAR is at the last position of the array, then the value
                  of REAR is set to 0 that corresponds to the first position of the array. */
-                if(REAR == max -1)
+                if (REAR == max - 1)
                     REAR = 0;
                 else
                     /*If REAR is not at the last position then its value is increment by one*/
-                    REAR = REAR +1;
+                    REAR = REAR + 1;
             }
             /*Once the position of REAR is deterined the element is added at its proper place.*/
             queue_array[REAR] = element;
@@ -48,7 +48,7 @@ namespace SampleCircular_A
         public void remove()
         {
             /*Checks whether the queue is empty*/
-            if(FRONT == -1)
+            if (FRONT == -1)
             {
                 Console.WriteLine("Queue underflow\n");
                 return;
@@ -56,7 +56,7 @@ namespace SampleCircular_A
             Console.WriteLine("\nThe element deleted from the queue is : " +
                 queue_array[FRONT] + "\n");
             /*Checks if the queue has one element*/
-            if(FRONT == REAR)
+            if (FRONT == REAR)
             {
                 FRONT = -1;
                 REAR = -1;
@@ -66,12 +66,12 @@ namespace SampleCircular_A
                 /*If the element to be deleted is at the last position
                  of the array, then the value of FRONT is set to 0 i.e
                 to the first element of the array*/
-                if(FRONT == max -1)
+                if (FRONT == max - 1)
                     FRONT = 0;
                 else
-                /*FRONT is increment by one if it is not the first element
-                 of array*/
-                FRONT = FRONT +1;   
+                    /*FRONT is increment by one if it is not the first element
+                     of array*/
+                    FRONT = FRONT + 1;
             }
         }
         public void display()
@@ -79,31 +79,93 @@ namespace SampleCircular_A
             int FRONT_position = FRONT;
             int REAR_position = REAR;
             /*Checks if the queue is empty */
-            if(FRONT == -1)
+            if (FRONT == -1)
             {
                 Console.WriteLine("Queue is empty\n");
                 return;
             }
             Console.WriteLine("\nElements in the queue are ................\n");
-            if(FRONT_position <= REAR_position)
+            if (FRONT_position <= REAR_position)
             {
                 /*traverses the queue till the last element present in an array*/
-                while(FRONT_position <= REAR_position)
+                while (FRONT_position <= REAR_position)
                 {
-                    Console.Write(queue_array[FRONT_position]+ "   " );
+                    Console.Write(queue_array[FRONT_position] + "   ");
                     FRONT_position++;
                 }
                 Console.WriteLine();
             }
             else
+            {
                 /*traverses the queue till the last position of the array*/
-                while(FRONT_position<= max - 1)
+                while (FRONT_position <= max - 1)
                 {
                     Console.Write(queue_array[FRONT_position] + "   ");
                     FRONT_position++;
                 }
-            Console.WriteLine();
+                /*Set the FRONT position to the first element of the array*/
+                FRONT_position = 0;
+                /*traverses the array till the last element present in the queue*/
+                while(FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "  ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+
         }
-        
+        static void Main(string[] args)
+        {
+            Queues q = new Queues();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("MENU");
+                    Console.WriteLine("1.Implemet insert operation");
+                    Console.WriteLine("2.Implement delete operation");
+                    Console.WriteLine("3.Display values");
+                    Console.WriteLine("4.Exit");
+                    Console.WriteLine("\nEnter your choice (1-4) :");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            Console.Write("\nEnter a Number : ");
+                            int num = Convert.ToInt32(System.Console.ReadLine());
+                            Console.WriteLine();
+                            q.insert(num);
+                            break;
+                        case '2':
+                            {
+                                q.remove();
+                            }
+                            break;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option!!");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered");
+                }
+               
+            }
+
+        }
     }
 }
+
